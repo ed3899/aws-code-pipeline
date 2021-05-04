aeb988554aadb1f2c9a139a42146a5892fb02c31
Edward Casanova <cb.inv7613@gmail.com>
2021-05-04 11:42:04 -0500

    Second commit

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,13 +1,45 @@
 import * as cdk from "@aws-cdk/core";
 import * as codecommit from "@aws-cdk/aws-codecommit";
+import * as codepipeline from "@aws-cdk/aws-codepipeline";
+import * as codepipeline_actions from "@aws-cdk/aws-codepipeline-actions";
+import { SimpleSynthAction, CdkPipeline } from "@aws-cdk/pipelines";
 
 export class WorkshopPipelineStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     //Create new code repo
-    new codecommit.Repository(this, "WorkshopRepo", {
+    const repo = new codecommit.Repository(this, "WorkshopRepo", {
       repositoryName: "WorkshopRepo",
+      description: "Some description",
+    });
+
+    //Defines the artifact representing the source code
+    const sourceArtifact = new codepipeline.Artifact();
+    // Defines the artifact representing the cloud assembly
+    // (cloudformation template + all other assets)
+    const cloudAssemblyArtifact = new codepipeline.Artifact();
+
+    // The basic pipeline declaration. This sets the initial structure
+    // of our pipeline
+
+    new CdkPipeline(this, "Pipeline", {
+      pipelineName: "WorkshopPipeline",
+      cloudAssemblyArtifact,
+
+      // Generates the source artifact from the repo we created in the last step
+      sourceAction: new codepipeline_actions.CodeCommitSourceAction({
+        actionName: "CodeCommit", // Any Git-based source control
+        repository: repo, // Designates the repo to draw code from
+        output: sourceArtifact, // Indicates where the artifact is stored
+      }),
+
+      // Builds our source code outlined above into a could assembly artifact
+      synthAction: SimpleSynthAction.standardNpmSynth({
+        sourceArtifact, // Where to get source code to build
+        cloudAssemblyArtifact, // Where to place built source
+        buildCommand: "npm run build",
+      }),
     });
   }
 }
